f068f31d1978177d5f6a75807a439e340913284c
Santosh Beyagudem <skbeyagu@gmail.com>
2015-11-10 17:05:24 -0500

    fixed typo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-var PORT = (process.env.PORT,3000);
+var PORT = (process.env.PORT || 3000);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res){
 	res.send('Todo API Root');
 
 });
