8f12990c616c259a31ba7c17bfa9bbe043af1241
Santosh Beyagudem <skbeyagu@gmail.com>
2015-11-12 14:51:36 -0500

    added sequelize post request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 
 var app = express();
 var PORT = (process.env.PORT || 3000);
@@ -64,18 +66,23 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function (todo){
+		res.json(todo.toJSON());
+	}, function (e){
+		res.status(400).json(e);
+	});
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	body.description = body.description.trim();
+	// body.description = body.description.trim();
 
-	body.id = todoNextId++;
+	// body.id = todoNextId++;
 
-	todos.push(body);
+	// todos.push(body);
 
 
-	res.json(body);
+	// res.json(body);
 });
 
 app.delete('/todos/:id', function(req, res) {
@@ -139,7 +146,8 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-
-app.listen(PORT, function() {
-	console.log('Express listen on port ' + PORT);
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listen on port ' + PORT);
+	});
 });
