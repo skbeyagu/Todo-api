36d1a3b9642ee625b36ba94cecc1f801d8478fd8
Santosh Kiran Beyagudem <skbeyagu@gmail.com>
2015-11-14 15:01:00 -0500

    added login route and authenicate class

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
-
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = (process.env.PORT || 3000);
@@ -196,13 +196,24 @@ app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function(e) {
 		res.status(400).json(e);
 	});
 });
 
-db.sequelize.sync(force: true).then(function() {
+// POST /users/login
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
+	});
+});
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listen on port ' + PORT);
 	});
