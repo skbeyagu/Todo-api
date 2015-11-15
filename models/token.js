eb388439ef8ad249c21a19be8a9a5e6a542e94b8
Santosh Kiran Beyagudem <skbeyagu@gmail.com>
2015-11-14 22:09:20 -0500

    added Delete users/login

---
+++ models/token.js
@@ -0,0 +1,21 @@
+var cryptojs = require('crypto-js');
+
+module.exports = function(sequelize, DataType) {
+
+	return sequelize.define('token', {
+		token: {
+			type: DataType.VIRTUAL,
+			allowNull: false,
+			validate: {
+				len: [1]
+			},
+			set: function(value) {
+				var hash = cryptojs.MD5(value).toString();
+
+				this.setDataValue('token', value);
+				this.setDataValue('tokenHash', hash);
+			}
+		},
+		tokenHash: DataType.STRING
+	});
+};
