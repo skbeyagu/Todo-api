906f6f06977b503fc83dbde9b25655ab0eb5635f
Santosh Kiran Beyagudem <skbeyagu@gmail.com>
2015-11-14 20:49:49 -0500

    added force true for db sync

--- server.js
+++ server.js
@@ -6,27 +6,33 @@ var bcrypt = require('bcrypt');
 var middleware = require('./middleware.js')(db);
 
 var app = express();
-var PORT = (process.env.PORT || 3000);
+var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
+
 app.use(bodyParser.json());
 
-// Get /todes
+app.get('/', function(req, res) {
+	res.send('Todo API Root');
+});
 
+// GET /todos?completed=false&q=work
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
-	var queryParams = req.query;
-	var where = {};
+	var query = req.query;
+	var where = {
+		userId: req.user.get('id')
+	};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
-			$like: '%' + queryParams.q + '%'
+			$like: '%' + query.q + '%'
 		};
 	}
 
@@ -37,39 +43,18 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	}, function(e) {
 		res.status(500).send();
 	});
-
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: false
-	// 	});
-	// }
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1
-	// 	})
-
-	// }
-
-
-
-	// res.json(filteredTodos);
 });
 
-app.get('/todos/:id',middleware.requireAuthentication, function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
-	var matchedID = _.findWhere(todos, {
-		id: todoID
-	});
+// GET /todos/:id
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
 
-
-	db.todo.findById(todoID).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -78,98 +63,50 @@ app.get('/todos/:id',middleware.requireAuthentication, function(req, res) {
 	}, function(e) {
 		res.status(500).send();
 	});
-	// todos.forEach(function (todo){
-	// 	if(todoID === todo.id){
-	// 		matchedID = todo;
-	// 	}
-	// });
-
-	// if (matchedID) {
-	// 	res.json(matchedID);
-
-	// } else {
-	// 	res.status(404).send();
-	// }
-
 });
 
-//POST /todos/
-
-app.post('/todos',middleware.requireAuthentication, function(req, res) {
+// POST /todos
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
-		//res.json(todo.toJSON());
-		req.user.addTodo(todo).then(function(){
+		req.user.addTodo(todo).then(function () {
 			return todo.reload();
-		}).then(function(todo){
+		}).then(function (todo) {
 			res.json(todo.toJSON());
 		});
 	}, function(e) {
 		res.status(400).json(e);
 	});
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-
-	// res.json(body);
 });
 
-app.delete('/todos/:id',middleware.requireAuthentication, function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
+// DELETE /todos/:id
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
 		where: {
-			id: todoID
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
-			if (rowsDeleted === 0) {
-				res.status(404).json({
-					error: 'no todo with id'
-				});
-			} else {
-				res.status(204).send();
-			}
-		},
-		function() {
-			res.status(500).send();
-		});
-
-
-
-	// var matchedID = _.findWhere(todos, {
-	// 	id: todoID
-	// });
-
-	// todos.forEach(function (todo){
-	// 	if(todoID === todo.id){
-	// 		matchedID = todo;
-	// 	}
-	// });
-
-	// if (matchedID) {
-	// 	res.json(matchedID);
-	// 	todos = _.without(todos, matchedID);
-	// } else {
-	// 	res.status(404).send();
-	// }
-
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function() {
+		res.status(500).send();
+	});
 });
 
-
-app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
+// PUT /todos/:id
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
-
-
 
 	if (body.hasOwnProperty('completed')) {
 		attributes.completed = body.completed;
@@ -179,31 +116,32 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoID).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
-
 			}, function(e) {
 				res.status(400).json(e);
 			});
 		} else {
 			res.status(404).send();
 		}
-
 	}, function() {
 		res.status(500).send();
 	});
-
-
 });
 
-app.post('/users', function(req, res) {
+app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	db.user.create(body).then(function(user) {
+	db.user.create(body).then(function (user) {
 		res.json(user.toPublicJSON());
-	}, function(e) {
+	}, function (e) {
 		res.status(400).json(e);
 	});
 });
@@ -214,12 +152,12 @@ app.post('/users/login', function (req, res) {
 
 	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
-		if (token){
-			res.header('Auth', token).json(user.toPublicJSON());
-		}else {
+
+		if (token) {
+			res.header('Auth', token).json(user.toPublicJSON());	
+		} else {
 			res.status(401).send();
 		}
-		
 	}, function () {
 		res.status(401).send();
 	});
@@ -227,6 +165,6 @@ app.post('/users/login', function (req, res) {
 
 db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
-		console.log('Express listen on port ' + PORT);
+		console.log('Express listening on port ' + PORT + '!');
 	});
 });
